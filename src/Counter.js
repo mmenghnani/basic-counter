08edadd03cc116f238ffa824d74141b63be4ec88
Mohit Menghnani <m.menghnani@gmail.com>
2019-02-06 23:15:14 -0800

    Push working version of the counter
    Push working version of the counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 
 export default class Counter extends Component {
+  constructor(){
+    super();
+    this.state = {
+      count : 1
+    }
+  }
   render() {
     return (
       <section className="Counter">
-        <h1>Count: {0}</h1>
-        <button onClick={() => {}} className="full-width">Increment</button>
-        <button onClick={() => {}} className="full-width">Decrement</button>
-        <button onClick={() => {}} className="full-width">Reset</button>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={() => {
+          this.setState({
+            count : this.state.count + 1
+          })
+        }} className="full-width">Increment</button>
+        <button onClick={() => {
+          this.setState({
+           count : this.state.count ? this.state.count - 1 : this.state.count
+          })
+        }} className="full-width">Decrement</button>
+        <button onClick={() => { this.setState({
+            count : 1
+          })}} className="full-width">Reset</button>
       </section>
     );
   }
